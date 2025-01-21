79e745b63ff3da3ce45afd1536cf1539b603906e
PureunKang <pureun0000@gmail.com>
2025-01-21 17:06:45 +0900

    feat : 프로젝트 설정

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <>
+      <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+        <h1 className="text-4xl font-bold text-blue-500 mb-4">
+          테일윈드및 데이지유아ㅣ 테스트
+        </h1>
+        <button className="btn">Button</button>
+      </div>
+    </>
+  );
+}
+
+export default App;
