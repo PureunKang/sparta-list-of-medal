9f82a4f3ad98c85ea6a66d4b527e548ba1bce9f2
PureunKang <pureun0000@gmail.com>
2025-01-22 23:36:18 +0900

    style : ui 되돌리기, 기존 기능 삭제

--- src/components/MedalInputForm.jsx
+++ src/components/MedalInputForm.jsx
@@ -1,49 +1,13 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
+import React, { useState } from "react";
 
 const MedalInputForm = () => {
-  const NATION_BASE_URL = "https://apis.data.go.kr/B410001/natnList/natnList";
-  const SERVICE_KEY = import.meta.env.VITE_SERVICE_KEY;
-
-  const [countries, setCountries] = useState([]);
-  const [selectedCountry, setSelectedCountry] = useState("");
-
-  const fetchNationData = async () => {
-    try {
-      const res = await axios.get(
-        `${NATION_BASE_URL}?serviceKey=${SERVICE_KEY}`
-      );
-      const nationItems = res.data.response.body.itemList.item;
-
-      const koreanNames = nationItems.map((n) => n.nationNmKor);
-      setCountries(koreanNames);
-    } catch (err) {
-      console.error(err);
-    }
-  };
-
-  useEffect(() => {
-    fetchNationData();
-  }, []);
-
   return (
-    <div className="form-control w-[80%] flex gap-2">
-      <select
-        className="select select-bordered max-w-xs"
-        value={selectedCountry}
-        onChange={(e) => {
-          setSelectedCountry(e.target.value);
-        }}
-      >
-        <option value="" disabled>
-          국가명
-        </option>
-        {countries.map((country, index) => (
-          <option key={index} value={country}>
-            {country}
-          </option>
-        ))}
-      </select>
+    <div className="form-control w-[80%] flex flex-row gap-2">
+      <input
+        type="string"
+        placeholder="국가명"
+        className="input input-bordered max-w-xs"
+      />
       <input
         type="number"
         placeholder="금메달"
