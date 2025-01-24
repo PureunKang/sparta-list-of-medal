f7af120cf7633417fcac489e1705af4b7747f0b0
PureunKang <pureun0000@gmail.com>
2025-01-24 15:09:03 +0900

    refactor : 모듈화 정정 최종

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,82 @@
+import React, { useState, useEffect } from "react";
 import MedalInputForm from "./components/MedalInputForm";
 import Header from "./components/Header";
+import MedalList from "./components/MedalList";
+import SortButton from "./components/SortButton";
+import Footer from "./components/Footer";
 
 function App() {
+  const [nations, setNations] = useState(() => {
+    const browserValue = localStorage.getItem("MEDAL");
+    if (!browserValue) return [];
+    else return JSON.parse(browserValue);
+  });
+
+  const [sortType, setSortType] = useState("gold");
+
+  useEffect(() => {
+    localStorage.setItem("MEDAL", JSON.stringify(nations));
+  }, [nations]);
+
+  const sortNations = (nationsList) => {
+    if (sortType === "gold") {
+      return nationsList.sort((a, b) => b.gold - a.gold);
+    } else {
+      return nationsList.sort(
+        (a, b) => b.gold + b.silver + b.bronze - (a.gold + a.silver + a.bronze)
+      );
+    }
+  };
+
+  const handleSortChange = (newSortType) => {
+    setSortType(newSortType);
+    setNations((prevNations) => sortNations([...prevNations]));
+  };
+
+  const addNation = (newNation) => {
+    const isNationExisted1 = nations.some((n) => n.nation === newNation.nation);
+
+    if (isNationExisted1) {
+      alert("해당 국가의 기록이 존재합니다. '업데이트'로 진행해 주세요.");
+      return;
+    }
+
+    const updatedNations = sortNations([...nations, newNation]);
+    setNations(updatedNations);
+  };
+
+  const removeNation = (id) => {
+    const updatedNations = sortNations(nations.filter((n) => n.id !== id));
+    setNations(updatedNations);
+  };
+
+  const updateNation = (updatedNation) => {
+    const isNationExisted2 = nations.some(
+      (n) => n.nation === updatedNation.nation
+    );
+
+    if (!isNationExisted2) {
+      alert("해당 국가의 기록이 존재하지 않습니다. '국가 추가'부터 해 주세요.");
+      return;
+    }
+
+    const updatedNations = sortNations(
+      nations.map((n) =>
+        n.nation === updatedNation.nation ? { ...n, ...updatedNation } : n
+      )
+    );
+    setNations(updatedNations);
+  };
+
   return (
     <>
       <Header />
-      <MedalInputForm />
+      <main>
+        <MedalInputForm onAddNation={addNation} onUpdateNation={updateNation} />
+        <SortButton sortType={sortType} onSortChange={handleSortChange} />
+        <MedalList nations={nations} onRemoveNation={removeNation} />
+      </main>
+      <Footer />
     </>
   );
 }
